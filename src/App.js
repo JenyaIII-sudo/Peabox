6f71969549ec1ba52d83132cd59b781a59f3b77d
Evgeniy Zabolotniy <evgeniyfirm.25@gmail.com>
2020-05-06 12:52:22 +0300

    Add smooth scrolling

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React from "react";
 import "./App.css";
 import HomePage from "./pages/homePage/HomePage.";
 import { BrowserRouter as Router } from "react-router-dom";
+import SmoothScrollbar from "smooth-scrollbar";
+import OverscrollPlugin from "smooth-scrollbar/plugins/overscroll";
+import Scrollbar from "react-smooth-scrollbar";
+
+SmoothScrollbar.use(OverscrollPlugin);
 
 const App = () => {
   return (
     <Router>
       <div className="App">
-        <HomePage />
+        <Scrollbar
+          style={{ height: "100vh" }}
+          damping={0.1}
+          thumbMinSize={20}
+          syncCallbacks={true}
+          renderByPixels={true}
+          alwaysShowTracks={true}
+          continuousScrolling={true}
+        >
+          <HomePage />
+        </Scrollbar>
       </div>
     </Router>
   );
